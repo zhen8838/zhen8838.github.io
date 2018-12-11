74bf293ee188a81fab9b300d1bf439eb01786a5e
zhen8838 <597323109@qq.com>
2018-12-11 16:48:46 +0800

    2018-12-11 16:48:43

--- js/script.js
+++ js/script.js
@@ -135,4 +135,14 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  
+  $(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+    });
+
 })(jQuery);
