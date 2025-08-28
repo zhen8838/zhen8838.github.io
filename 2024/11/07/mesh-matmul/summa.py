f548dda5cbedb7d60429c73542c1ab563877bfd7
QiHang Zheng <597323109@qq.com>
2025-08-28 12:03:18 +0800

    YYYY-08-Aug 28, 2025 12:03:16

--- 2024/11/07/mesh-matmul/summa.py
+++ 2024/11/07/mesh-matmul/summa.py
@@ -8,13 +8,13 @@ def matmul(c: np.ndarray, a, b):
 
 @log_sparse(stack_depth=3)
 def summa(A: np.ndarray, B: np.ndarray, C: np.ndarray, P, comm2d: MPI.Cartcomm):
-  col_comm = comm2d.Sub([True, False])
+  col_comm = comm2d.Sub([True, False]) # [y,x]
   row_comm = comm2d.Sub([False, True])
 
   (M, K) = A.shape
   N = B.shape[1]
   (mTile, nTile, kTile) = (M // P, N // P, K // P)
-  (j, i) = comm2d.Get_coords(rank)  # topology is row major
+  (j, i) = comm2d.Get_coords(rank)  # topology is row major, [y,x]
   # align data
   a = np.ascontiguousarray(A[j * mTile:(j + 1) * mTile, i * kTile:(i + 1) * kTile].copy())
   b = np.ascontiguousarray(B[j * kTile:(j + 1) * kTile, i * nTile:(i + 1) * nTile].copy())
@@ -24,7 +24,7 @@ def summa(A: np.ndarray, B: np.ndarray, C: np.ndarray, P, comm2d: MPI.Cartcomm):
   for k in range(P):
     Atemp = a if k == i else np.empty_like(a)
     Btemp = b if k == j else np.empty_like(b)
-    row_comm.Bcast(Atemp, root=k)
+    row_comm.Bcast(Atemp, root=k) # A是 [m@x,k@y]，把y的数据进行分发时，需要把[y@false,x@true]，也就是另一路设置为true。
     col_comm.Bcast(Btemp, root=k)
     matmul(c.view(), Atemp, Btemp)
 
